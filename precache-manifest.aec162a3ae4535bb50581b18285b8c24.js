fc0d7ffdc53c44a7611430f523a50ccff9c2f6c9
kheva <khevamann@gmail.com>
2020-03-30 19:38:21 -0700

    Updates

---
+++ precache-manifest.aec162a3ae4535bb50581b18285b8c24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbf2131b30f912849ad197d0250010e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c7b3b04020136040eea",
+    "url": "/static/css/main.68ad485b.chunk.css"
+  },
+  {
+    "revision": "6de4d2a4fdb46e15aeb5",
+    "url": "/static/js/2.d3a54b77.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.d3a54b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7b3b04020136040eea",
+    "url": "/static/js/main.d875a355.chunk.js"
+  },
+  {
+    "revision": "9d6ae8d7bd21cb13e581",
+    "url": "/static/js/runtime-main.d88144b0.js"
+  }
+]);
