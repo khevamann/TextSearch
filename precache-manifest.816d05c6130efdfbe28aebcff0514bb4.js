c4839c4ca53ff1271ff154241e54e04e7f5fc6c9
khevamann <khevamann@users.noreply.github.com>
2020-03-31 04:05:44 +0000

    deploy: f87e8cbdb8e9bea2e2ed08161918164080ca1cfe

---
+++ precache-manifest.816d05c6130efdfbe28aebcff0514bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1b2ea7c28bf696c8bb1543d3e08277",
+    "url": "/text-search/index.html"
+  },
+  {
+    "revision": "7239d6a58dcc2271a1ce",
+    "url": "/text-search/static/css/main.686ac02f.chunk.css"
+  },
+  {
+    "revision": "6de4d2a4fdb46e15aeb5",
+    "url": "/text-search/static/js/2.d3a54b77.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/text-search/static/js/2.d3a54b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7239d6a58dcc2271a1ce",
+    "url": "/text-search/static/js/main.a12a3963.chunk.js"
+  },
+  {
+    "revision": "334024e818ae74bfd059",
+    "url": "/text-search/static/js/runtime-main.092843e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9196c1548a95007556f621273fe1cd73.js"
+  "/text-search/precache-manifest.816d05c6130efdfbe28aebcff0514bb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/text-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
